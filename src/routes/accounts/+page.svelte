7df407097151443d02089bad85780c74d5779c90
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2023-09-17 13:21:33 +0800

    add account type

--- src/routes/accounts/+page.svelte
+++ src/routes/accounts/+page.svelte
@@ -7,6 +7,9 @@
 
   export let data;
 
+  let loading = false;
+  let accountTypeName = '';
+
   const accountTypesQuery = createQuery({
     queryKey: ['account_types'],
     queryFn: async () => {
@@ -15,7 +18,13 @@
     }
   });
 
-  $accountTypesQuery;
+  async function addAccountType() {
+    loading = true;
+    await axios.post(`${data.apiHost}/account_types`, { name: accountTypeName });
+    accountTypeName = '';
+    loading = false;
+    $accountTypesQuery.refetch();
+  }
 </script>
 
 <div class="flex flex-col gap-5">
@@ -24,15 +33,17 @@
   <div class="flex flex-col gap-3">
     <h1 class="text-xl font-bold">Account Types</h1>
 
-    <div class="flex gap-2">
-      <Input placeholder="Add a new account type" />
-      <Button variant="outline">Add</Button>
-    </div>
+    <form class="flex gap-2" on:submit={addAccountType}>
+      <Input placeholder="Add a new account type" bind:value={accountTypeName} />
+      <Button variant="outline" disabled={loading}>Add</Button>
+    </form>
 
-    <div>
+    <div class="flex flex-col gap-3">
       {#if $accountTypesQuery.data}
         {#each $accountTypesQuery.data as accountType}
-          <p>{accountType.name}</p>
+          <div class="rounded-md border border-slate-800 p-3">
+            <p>{accountType.name}</p>
+          </div>
         {/each}
       {/if}
     </div>
