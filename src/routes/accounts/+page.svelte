a240f53f85d96efdd7fc472a4439d4578f6e55cb
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2023-09-17 13:40:33 +0800

    delete account type

--- src/routes/accounts/+page.svelte
+++ src/routes/accounts/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
   import type { AccountType } from '$lib/types.js';
-  import { createQuery } from '@tanstack/svelte-query';
+  import { createMutation, createQuery, useQueryClient } from '@tanstack/svelte-query';
   import axios from 'axios';
   import { Input } from '$lib/components/ui/input';
   import { Button } from '$lib/components/ui/button';
+  import { Trash2 } from 'lucide-svelte';
 
   export let data;
 
@@ -15,6 +16,31 @@
     queryFn: async () => {
       const response = await axios.get(`${data.apiHost}/account_types`);
       return response.data as AccountType[];
+    }
+  });
+
+  const client = useQueryClient();
+
+  const deleteAccountTypeMutation = createMutation({
+    mutationKey: ['delete', 'account_type'],
+    mutationFn: async (accountType: AccountType) => {
+      await axios.delete(`${data.apiHost}/account_types/${accountType.id}`);
+    },
+    onMutate: async (accountType: AccountType) => {
+      await client.cancelQueries(['account_types']);
+
+      const currentAccountTypes = client.getQueryData<AccountType[]>(['account_types']);
+
+      const updatedAccountTypes = currentAccountTypes?.filter(
+        (a: AccountType) => a.id !== accountType.id
+      );
+
+      client.setQueryData<AccountType[]>(['account_types'], []); // this forces a re-render
+      client.setQueryData<AccountType[]>(['account_types'], updatedAccountTypes);
+    },
+
+    onSettled: () => {
+      client.invalidateQueries(['account_types']);
     }
   });
 
@@ -41,8 +67,16 @@
     <div class="flex flex-col gap-3">
       {#if $accountTypesQuery.data}
         {#each $accountTypesQuery.data as accountType}
-          <div class="rounded-md border border-slate-800 p-3">
+          <div
+            class="flex place-content-between place-items-center rounded-md border border-slate-800 px-5 py-2">
             <p>{accountType.name}</p>
+            <Button
+              variant="ghost"
+              size="icon"
+              class="group hover:bg-destructive"
+              on:click={() => {
+                $deleteAccountTypeMutation.mutate(accountType);
+              }}><Trash2 class="h-5 w-5 text-zinc-500 group-hover:text-red-300" /></Button>
           </div>
         {/each}
       {/if}
