fb1def7561c7bbc4144c1062c0ec5e9612c1fa5f
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2023-09-19 16:18:04 +0800

    transactions page

--- src/routes/transactions/+page.svelte
+++ src/routes/transactions/+page.svelte
@@ -1,1 +1,10 @@
-<h1>Transactions</h1>
+<script lang="ts">
+  import Button from '$lib/components/ui/button/button.svelte';
+</script>
+
+<div class="flex flex-col gap-5">
+  <div class="flex place-content-between place-items-center">
+    <h1 class="text-xl font-bold">Transactions</h1>
+    <Button class="h-fit">Add</Button>
+  </div>
+</div>
