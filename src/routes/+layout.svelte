7e2e7fc78bd18eea306db2dab3cc2a942867cb66
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2023-09-20 18:35:50 +0800

    apply bg gradient

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -19,35 +19,37 @@
   <title>Budgeteer</title>
 </svelte:head>
 <QueryClientProvider client={queryClient}>
-  <div class="flex min-h-screen place-content-center bg-black">
-    <div class="flex flex-col gap-5 pt-5 xl:w-2/6">
-      <div class="flex place-content-between place-items-center">
-        <h1 class="text-4xl font-bold tracking-tight text-white">Budgeteer</h1>
+  <div class="flex min-h-screen place-content-center bg-gradient-to-bl from-purple-500 to-cyan-500">
+    <div class="flex min-h-screen w-full place-content-center bg-black opacity-75">
+      <div class="flex flex-col gap-5 pt-5 xl:w-2/6">
+        <div class="flex place-content-between place-items-center">
+          <h1 class="text-4xl font-bold tracking-tight text-white">Budgeteer</h1>
 
-        <div class="flex gap-5">
-          <a
-            href="/budget"
-            class={twMerge(
-              'underline-offset-4 transition-all duration-200 hover:underline',
-              $page.url.pathname === '/budget' ? ' text-primary underline' : 'text-zinc-400'
-            )}>Budget</a>
-          <a
-            href="/transactions"
-            class={twMerge(
-              'underline-offset-4 transition-all duration-200 hover:underline',
-              $page.url.pathname === '/transactions' ? ' text-primary underline' : 'text-zinc-400'
-            )}>Transactions</a>
-          <a
-            href="/accounts"
-            class={twMerge(
-              'underline-offset-4 transition-all duration-200 hover:underline',
-              $page.url.pathname === '/accounts' ? ' text-primary underline' : 'text-zinc-400'
-            )}>Accounts</a>
+          <div class="flex gap-5">
+            <a
+              href="/budget"
+              class={twMerge(
+                'underline-offset-4 transition-all duration-200 hover:underline',
+                $page.url.pathname === '/budget' ? ' text-primary underline' : 'text-zinc-400'
+              )}>Budget</a>
+            <a
+              href="/transactions"
+              class={twMerge(
+                'underline-offset-4 transition-all duration-200 hover:underline',
+                $page.url.pathname === '/transactions' ? ' text-primary underline' : 'text-zinc-400'
+              )}>Transactions</a>
+            <a
+              href="/accounts"
+              class={twMerge(
+                'underline-offset-4 transition-all duration-200 hover:underline',
+                $page.url.pathname === '/accounts' ? ' text-primary underline' : 'text-zinc-400'
+              )}>Accounts</a>
+          </div>
         </div>
-      </div>
 
-      <div class="rounded-lg bg-zinc-950 p-10">
-        <slot />
+        <div class="mb-10 rounded-lg bg-zinc-950 p-10">
+          <slot />
+        </div>
       </div>
     </div>
   </div>
